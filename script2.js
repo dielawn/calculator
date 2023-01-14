835a99f84576ae39cc453025dcce28925d21bf69
dielawn <dielawnmerkill@gmail.com>
2023-01-14 07:56:36 -0700

    decimal is working

--- script2.js
+++ script2.js
@@ -3,20 +3,14 @@ let operatorBtns = document.querySelectorAll('.operator')
 let numberBtns = document.querySelectorAll('.number')
 let equalBtn = document.querySelector('#equal')
 let clearBtn = document.querySelector('#clear')
-
-
-
 let solvedInput = document.getElementById('solvedInput')
 let input = document.getElementById('input')
-
-
 let num1 = null;
 let num2 = null;
 let operator = null;
 let previousOperator = null;
 let result = getResult(Number(num1), operator, Number(num2));
 
-//sets content of inputs
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -24,11 +18,7 @@ const divide = (a, b) => a / b;
 
 let currentOperator = null;
 
-let pendingResult = null;
-
 function getOperatorNums(event) {
-    num1 = Number(num1)
-    num2 = Number(num2)
     if(event.target.matches('button')) {
         const action = event.target.getAttribute('data-action');
         if((action === 'add') || 
@@ -38,14 +28,11 @@ function getOperatorNums(event) {
             if(operator != null) {
                 let previousOperator = operator
                 result = getResult(num1, operator, num2)
-                operator = event.target.textContent
-                
+                operator = event.target.textContent                
                 solvedInput.value = `${Number(num1)} ${previousOperator} ${Number(num2)} =`;
                 num1 = input.value;
-                num2 = null;
-                
-             
-        }
+                num2 = null;             
+            }
             operator = event.target.textContent;
         }
     }
@@ -54,45 +41,38 @@ function getOperatorNums(event) {
             if (num2 === null) {
                 num2 = 0;
             }
-            num2 += event.target.textContent;
+            num2 += event.target.textContent; 
         } else {
             if (num1 === null) {
                 num1 = 0;
             }
-            num1 += event.target.textContent;
+            num1 += event.target.textContent;           
         }
     }
 }
 
-
-
 function getResult(num1, operator, num2){
-    console.log(num1)
     num1 = Number(num1)
     num2 = Number(num2)
-    console.log(num1)
     let result;
     if(operator === '+'){
-        let result = add(num1, num2) 
-        input.value = result  
-               
+        result = add(num1, num2) 
+        input.value = result         
     }
     if (operator === '-'){
-        let result = subtract(num1, num2)
+        result = subtract(num1, num2)
         input.value = result
     }
     if (operator === '*'){
-        let result = multiply(num1, num2)
+        result = multiply(num1, num2)
         input.value = result
     }
-    if (operator === '/'){
-        let result = divide(num1, num2)
+    if (operator === '/'){       
+        result = divide(num1, num2)
         input.value = result
     }    
     return result
 }
-
-
 
 function display(event) {
     const key = event.target.textContent
@@ -101,9 +81,7 @@ function display(event) {
     }
     const action = event.target.getAttribute('data-action')
     if (action === 'solve') {
-        result = getResult(num1, operator, num2);
-        console.log(getResult(num1, operator, num2))
-        
+        result = getResult(num1, operator, num2);        
     }
     if (action === 'clear') {
         clear()
@@ -118,32 +96,18 @@ function clear() {
     result = null;
     operator = null;
     previousOperator = null;
-
 }
 
 allButtons.forEach(button => {
     button.addEventListener('click', event => {
-        
         getOperatorNums(event);
         display(event);
         if(button.textContent === '='){    
             solvedInput.value = `${Number(num1)} ${operator} ${Number(num2)} =`;
             num1 = input.value;
             num2 = 0;   
-           
-           previousOperator = operator
-           operator = null;
-           console.log(solvedInput.value, input.value)
-                 
-
-        }
-        
-        
-    
+            previousOperator = operator
+            operator = null;
+        }    
     });
 });
-
-
-
-
-
